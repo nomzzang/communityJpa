86eb78e6d80e740052dca376d1d0fbea6ce06506
kyu <starnomzzang@naver.com>
2023-05-21 21:18:52 +0900

    Jpa

---
+++ src/main/java/com/example/communityjpa/CommunityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommunityJpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/communityjpa/CommunityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
